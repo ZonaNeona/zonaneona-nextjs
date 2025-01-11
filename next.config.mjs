60a4968ea5b18613d78a4ca8d9ef2a4039659ff6
Igor <111438916+ZonaNeona@users.noreply.github.com>
2025-01-11 22:45:56 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
-};
-
-export default nextConfig;
-
-export default {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'neonfest.ru',
-        port: '',
-        pathname: '/media/**',
+        protocol: "https",
+        hostname: "neonfest.ru",
+        port: "",
+        pathname: "/media/**",
       },
     ],
   },
 };
+
+export default nextConfig;
