7ca5498a0faf9f16eb8a7e6fb877fb8a16b4ae6e
Igor <111438916+ZonaNeona@users.noreply.github.com>
2025-01-12 17:01:44 +0300

    Update Category-Banner.js

--- components/Category/Category-Banner.js
+++ components/Category/Category-Banner.js
@@ -1,47 +1,44 @@
 import Link from "next/link";
 
 const CategoryBanner = ({ category }) => {
+  const categoryTitle = category?.category || "Все курсы";
+  const courseCount = category?.length || 0;
+  const categoryDescription =
+    category?.desc ||
+    "Ознакомьтесь с нашими курсами, которые помогут вам стать экспертом в своей области.";
+
   return (
     <>
       <div className="rbt-banner-content-top">
         <div className="container">
-          {category && (
-            <div className="row">
-              <div className="col-lg-12">
-                <ul className="page-list">
-                  <li className="rbt-breadcrumb-item">
-                    <Link href="/">Home</Link>
-                  </li>
-                  <li>
-                    <div className="icon-right">
-                      <i className="feather-chevron-right"></i>
-                    </div>
-                  </li>
-                  <li className="rbt-breadcrumb-item active">
-                    {category && category.category
-                      ? category.category
-                      : "All Courses"}
-                  </li>
-                </ul>
-                <div className=" title-wrapper">
-                  <h1 className="title mb--0">
-                    {category && category.category
-                      ? category.category
-                      : "All Courses"}
-                  </h1>
-                  <Link href="#" className="rbt-badge-2">
-                    <div className="image">🎉</div>
-                    {category && category.id} {category.length} Courses
-                  </Link>
-                </div>
-                <p className="description">
-                  {category && category.desc
-                    ? category.desc
-                    : "Courses that help beginner designers become true unicorns."}
-                </p>
+          <div className="row">
+            <div className="col-lg-12">
+              {/* Хлебные крошки */}
+              <ul className="page-list">
+                <li className="rbt-breadcrumb-item">
+                  <Link href="/">Главная</Link>
+                </li>
+                <li>
+                  <div className="icon-right">
+                    <i className="feather-chevron-right"></i>
+                  </div>
+                </li>
+                <li className="rbt-breadcrumb-item active">{categoryTitle}</li>
+              </ul>
+
+              {/* Заголовок и количество курсов */}
+              <div className="title-wrapper">
+                <h1 className="title mb--0">{categoryTitle}</h1>
+                <Link href="#" className="rbt-badge-2">
+                  <div className="image">🎉</div>
+                  {courseCount} Курс{courseCount === 1 ? "" : "ов"}
+                </Link>
               </div>
+
+              {/* Описание категории */}
+              <p className="description">{categoryDescription}</p>
             </div>
-          )}
+          </div>
         </div>
       </div>
     </>
