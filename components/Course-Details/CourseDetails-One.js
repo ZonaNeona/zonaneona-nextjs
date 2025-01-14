14eb2a7f4659160efbe71f75385289d553c62090
Igor <111438916+ZonaNeona@users.noreply.github.com>
2025-01-14 18:22:03 +0300

    Update CourseDetails-One.js

--- components/Course-Details/CourseDetails-One.js
+++ components/Course-Details/CourseDetails-One.js
@@ -4,7 +4,7 @@ import CourseMenu from "./Course-Sections/Course-Menu";
 import Featured from "./Course-Sections/Featured";
 import Instructor from "./Course-Sections/Instructor";
 import Overview from "./Course-Sections/Overview";
-import RelatedCourse from "./Course-Sections/RelatedCourse";
+import SimilarCourses from "./Course-Sections/SimilarCourses";
 import Requirements from "./Course-Sections/Requirements";
 import Review from "./Course-Sections/Review";
 import Video from "./Course-Sections/Viedo";
@@ -22,58 +22,46 @@ const CourseDetailsOne = ({ checkMatchCourses }) => {
     <>
       <div className="col-lg-8">
         <div className="course-details-content">
-          {/* Баннер курса */}
           {checkMatchCourses.courseImg && (
             <div className="rbt-course-feature-box rbt-shadow-box thumbnail">
               <CourseBanner bannerImg={checkMatchCourses.courseImg} />
             </div>
           )}
 
-          {/* Меню страницы */}
           <div className="rbt-inner-onepage-navigation sticky-top mt--30">
             <CourseMenu />
           </div>
 
-          {/* Описание курса */}
           {checkMatchCourses.courseOverview && (
             <div className="course-overview mt--30">
-              {checkMatchCourses.courseOverview.map((data, index) => (
-                <Overview {...data} key={index} checkMatchCourses={data} />
+              {checkMatchCourses.courseOverview.map((overview, index) => (
+                <Overview key={index} checkMatchCourses={overview} />
               ))}
             </div>
           )}
 
-          {/* Содержимое курса */}
-          {checkMatchCourses.courseContent && (
+          {checkMatchCourses.modules && (
             <div
               className="course-content rbt-shadow-box coursecontent-wrapper mt--30"
               id="coursecontent"
             >
-              {checkMatchCourses.courseContent.map((data, index) => (
-                <Content {...data} key={index} checkMatchCourses={data} />
-              ))}
+              <Content checkMatchCourses={checkMatchCourses.modules} />
             </div>
           )}
 
-          {/* Требования курса */}
           {checkMatchCourses.courseRequirement && (
             <div
               className="rbt-course-feature-box rbt-shadow-box details-wrapper mt--30"
               id="details"
             >
               <div className="row g-5">
-                {checkMatchCourses.courseRequirement.map((data, index) => (
-                  <Requirements
-                    {...data}
-                    key={index}
-                    checkMatchCourses={data}
-                  />
+                {checkMatchCourses.courseRequirement.map((req, index) => (
+                  <Requirements key={index} checkMatchCourses={req} />
                 ))}
               </div>
             </div>
           )}
 
-          {/* Инструктор */}
           {checkMatchCourses.courseInstructor && (
             <div
               className="rbt-instructor rbt-shadow-box intructor-wrapper mt--30"
@@ -83,36 +71,22 @@ const CourseDetailsOne = ({ checkMatchCourses }) => {
             </div>
           )}
 
-          {/* Отзывы */}
-          <div
-            className="rbt-review-wrapper rbt-shadow-box review-wrapper mt--30"
-            id="review"
-          >
-            <Review />
-          </div>
-
-          {/* Избранные отзывы 
           {checkMatchCourses.featuredReview && (
             <div className="rbt-featured-reviews mt--30">
-              {checkMatchCourses.featuredReview.map((data, index) => (
-                <Featured {...data} key={index} coursesFeatured={data} />
+              {checkMatchCourses.featuredReview.map((review, index) => (
+                <Featured key={index} coursesFeatured={review} />
               ))}
             </div>
           )}
-            */}
         </div>
 
-        {/* Связанные курсы */}
         {checkMatchCourses.relatedCourse && (
           <div className="related-course mt--60">
-            {checkMatchCourses.relatedCourse.map((data, index) => (
-              <RelatedCourse {...data} key={index} checkMatchCourses={data} />
-            ))}
+            <SimilarCourses checkMatchCourses={checkMatchCourses.relatedCourse} />
           </div>
         )}
       </div>
 
-      {/* Боковая панель */}
       <div className="col-lg-4">
         <div className="course-sidebar sticky-top rbt-shadow-box course-sidebar-top rbt-gradient-border">
           <div className="inner">
