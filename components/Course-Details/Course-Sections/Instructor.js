03bea2a64a169a759a32fb21a3f9d2593314b73f
Igor <111438916+ZonaNeona@users.noreply.github.com>
2025-01-14 16:56:47 +0300

    Update Instructor.js

--- components/Course-Details/Course-Sections/Instructor.js
+++ components/Course-Details/Course-Sections/Instructor.js
@@ -1,74 +1,47 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
-const Instructor = ({ checkMatchCourses }) => {
-  return (
-    <>
-      <div className="about-author border-0 pb--0 pt--0">
-        <div className="section-title mb--30">
-          <h4 className="rbt-title-style-3">{checkMatchCourses.title}</h4>
-        </div>
-        {checkMatchCourses.body.map((teacher, innerIndex) => (
-          <div className="media align-items-center" key={innerIndex}>
-            <div className="thumbnail">
-              <Link href={`/profile/${teacher.id}`}>
-                <Image
-                  src={teacher.img}
-                  width={250}
-                  height={250}
-                  alt="Author Images"
-                />
-              </Link>
-            </div>
-            <div className="media-body">
-              <div className="author-info">
-                <h5 className="title">
-                  <Link
-                    className="hover-flip-item-wrapper"
-                    href={`/profile/${teacher.id}`}
-                  >
-                    {teacher.name}
-                  </Link>
-                </h5>
-                <span className="b3 subtitle">{teacher.type}</span>
-                <ul className="rbt-meta mb--20 mt--10">
-                  <li>
-                    <i className="fa fa-star color-warning"></i>
-                    {teacher.ratingNumber} Reviews
-                    <span className="rbt-badge-5 ml--5">
-                      {teacher.star} Rating
-                    </span>
-                  </li>
-                  <li>
-                    <i className="feather-users"></i> {teacher.studentNumber}
-                    Students
-                  </li>
-                  <li>
-                    <Link href="#">
-                      <i className="feather-video"></i> {teacher.course} Courses
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-              <div className="content">
-                <p className="description">{teacher.desc}</p>
+const Instructor = ({ instructor }) => {
+  if (!instructor) {
+    return <div>Информация об эксперте отсутствует.</div>;
+  }
 
-                <ul className="social-icon social-default icon-naked justify-content-start">
-                  {teacher.social.map((social, index) => (
-                    <li key={index}>
-                      <Link href={social.link}>
-                        <i className={`feather-${social.icon}`}></i>
-                      </Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          </div>
-        ))}
+  return (
+    <div className="about-author border-0 pb--0 pt--0">
+      <div className="section-title mb--30">
+        <h4 className="rbt-title-style-3">Эксперт курса</h4>
       </div>
-    </>
+      <div className="media align-items-center">
+        <div className="thumbnail">
+          <Image
+            src={instructor.img}
+            width={250}
+            height={250}
+            alt="Фото эксперта"
+          />
+        </div>
+        <div className="media-body">
+          <div className="author-info">
+            <h5 className="title">{instructor.name}</h5>
+            <span className="b3 subtitle">{instructor.type}</span>
+          </div>
+          <div className="content">
+            <p className="description">{instructor.desc}</p>
+            <ul className="social-icon social-default icon-naked justify-content-start">
+              {/* Здесь можно добавить социальные ссылки, если они есть */}
+              <li>
+                <Link href={`tel:${instructor.contact}`}>
+                  <i className="feather-phone"></i> Связаться
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
