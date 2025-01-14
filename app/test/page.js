e6b619ebeed00c97510afda291d92d8e244f5db2
Igor <111438916+ZonaNeona@users.noreply.github.com>
2025-01-14 16:40:34 +0300

    Update page.js

--- app/test/page.js
+++ app/test/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import Content from "../../components/Course-Details/Course-Sections/Course-Menu";
+import Content from "../../components/Course-Details/Course-Sections/Featured";
 
 
 const TestPage = () => {
