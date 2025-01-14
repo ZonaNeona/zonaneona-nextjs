37f08272c757b2c8409449bd72230c26d2b9b6e8
Igor <111438916+ZonaNeona@users.noreply.github.com>
2025-01-14 17:13:32 +0300

    Update Overview.js

--- components/Course-Details/Course-Sections/Overview.js
+++ components/Course-Details/Course-Sections/Overview.js
@@ -4,55 +4,58 @@ import React, { useState } from "react";
 
 const Overview = ({ checkMatchCourses }) => {
   const [toggle, setToggle] = useState(false);
+
+  if (!checkMatchCourses) {
+    return <div>Данные для обзора курса отсутствуют.</div>;
+  }
+
   const { title, desc, descTwo, overviewList } = checkMatchCourses;
 
   return (
-    <>
-      <div
-        className={`rbt-course-feature-box overview-wrapper rbt-shadow-box mt--30 has-show-more ${
-          toggle ? "active" : ""
-        }`}
-        id="overview"
-      >
-        <div className="rbt-course-feature-inner has-show-more-inner-content">
-          <div className="section-title">
-            <h4 className="rbt-title-style-3">{title}</h4>
-          </div>
-          <p>{desc}</p>
-
-          <div className="row g-5 mb--30">
-            <div className="col-lg-6">
-              <ul className="rbt-list-style-1">
-                {overviewList.slice(0, 4).map((item, innerIndex) => (
-                  <li key={innerIndex}>
-                    <i className="feather-check"></i>
-                    {item.listItem}
-                  </li>
-                ))}
-              </ul>
-            </div>
-
-            <div className="col-lg-6">
-              <ul className="rbt-list-style-1">
-                {overviewList.slice(4, 8).map((item, innerIndex) => (
-                  <li key={innerIndex}>
-                    <i className="feather-check"></i>
-                    {item.listItem}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-          <p>{descTwo}</p>
+    <div
+      className={`rbt-course-feature-box overview-wrapper rbt-shadow-box mt--30 has-show-more ${
+        toggle ? "active" : ""
+      }`}
+      id="overview"
+    >
+      <div className="rbt-course-feature-inner has-show-more-inner-content">
+        <div className="section-title">
+          <h4 className="rbt-title-style-3">{title}</h4>
         </div>
-        <div
-          className={`rbt-show-more-btn ${toggle ? "active" : ""}`}
-          onClick={() => setToggle(!toggle)}
-        >
-          Show More
+        <p>{desc}</p>
+
+        <div className="row g-5 mb--30">
+          <div className="col-lg-6">
+            <ul className="rbt-list-style-1">
+              {overviewList.slice(0, 4).map((item, innerIndex) => (
+                <li key={innerIndex}>
+                  <i className="feather-check"></i>
+                  {item.listItem}
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="col-lg-6">
+            <ul className="rbt-list-style-1">
+              {overviewList.slice(4, 8).map((item, innerIndex) => (
+                <li key={innerIndex}>
+                  <i className="feather-check"></i>
+                  {item.listItem}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
+        <p>{descTwo}</p>
       </div>
-    </>
+      <div
+        className={`rbt-show-more-btn ${toggle ? "active" : ""}`}
+        onClick={() => setToggle(!toggle)}
+      >
+        Show More
+      </div>
+    </div>
   );
 };
 
