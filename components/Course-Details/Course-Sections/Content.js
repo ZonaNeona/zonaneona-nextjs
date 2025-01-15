82966eff04fb2c4673408e5c8b06783866b4ef79
Igor <111438916+ZonaNeona@users.noreply.github.com>
2025-01-15 05:38:40 +0300

    Update Content.js

--- components/Course-Details/Course-Sections/Content.js
+++ components/Course-Details/Course-Sections/Content.js
@@ -2,7 +2,10 @@ import Link from "next/link";
 import React from "react";
 
 const Content = ({ checkMatchCourses }) => {
-  if (!checkMatchCourses) return <p>Нет данных для отображения</p>;
+  // Проверяем наличие данных
+  if (!checkMatchCourses || !checkMatchCourses.modules || checkMatchCourses.modules.length === 0) {
+    return <p>Модули отсутствуют</p>;
+  }
 
   return (
     <div className="rbt-course-feature-inner">
@@ -12,61 +15,59 @@ const Content = ({ checkMatchCourses }) => {
 
       <div className="rbt-accordion-style rbt-accordion-02 accordion">
         <div className="accordion" id="accordionExampleb2">
-          {checkMatchCourses.modules &&
-            checkMatchCourses.modules.map((module, innerIndex) => (
-              <div className="accordion-item card" key={innerIndex}>
-                <h2
-                  className="accordion-header card-header"
-                  id={`headingTwo${innerIndex}`}
-                >
-                  <button
-                    className={`accordion-button ${
-                      !module.collapsed ? "collapsed" : ""
-                    }`}
-                    type="button"
-                    data-bs-toggle="collapse"
-                    data-bs-target={`#collapseTwo${innerIndex}`}
-                    aria-expanded={module.expand}
-                    aria-controls={`collapseTwo${innerIndex}`}
-                  >
-                    {module.title}
-                    <span className="rbt-badge-5 ml--10">{module.time}</span>
-                  </button>
-                </h2>
-
-                <div
-                  id={`collapseTwo${innerIndex}`}
-                  className={`accordion-collapse collapse ${
-                    module.isShow ? "show" : ""
+          {checkMatchCourses.modules.map((module, innerIndex) => (
+            <div className="accordion-item card" key={innerIndex}>
+              <h2
+                className="accordion-header card-header"
+                id={`headingTwo${innerIndex}`}
+              >
+                <button
+                  className={`accordion-button ${
+                    !module.collapsed ? "collapsed" : ""
                   }`}
-                  aria-labelledby={`headingTwo${innerIndex}`}
-                  data-bs-parent="#accordionExampleb2"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapseTwo${innerIndex}`}
+                  aria-expanded={module.expand}
+                  aria-controls={`collapseTwo${innerIndex}`}
                 >
-                  <div className="accordion-body card-body pr--0">
-                    <ul className="rbt-course-main-content liststyle">
-                      {module.lessons &&
-                        module.lessons.map((lesson, subIndex) => (
-                          <li key={subIndex}>
-                            <Link href="/lesson">
-                              <div className="course-content-left">
-                                {lesson.isFree ? (
-                                  <i className="feather-play-circle"></i>
-                                ) : (
-                                  <i className="feather-lock"></i>
-                                )}
-                                <span className="text">{lesson.title}</span>
-                              </div>
-                              <div className="course-content-right">
-                                <span className="min-lable">{lesson.time}</span>
-                              </div>
-                            </Link>
-                          </li>
-                        ))}
-                    </ul>
-                  </div>
+                  {module.title}
+                  <span className="rbt-badge-5 ml--10">{module.time}</span>
+                </button>
+              </h2>
+
+              <div
+                id={`collapseTwo${innerIndex}`}
+                className={`accordion-collapse collapse ${
+                  module.isShow ? "show" : ""
+                }`}
+                aria-labelledby={`headingTwo${innerIndex}`}
+                data-bs-parent="#accordionExampleb2"
+              >
+                <div className="accordion-body card-body pr--0">
+                  <ul className="rbt-course-main-content liststyle">
+                    {module.lessons.map((lesson, subIndex) => (
+                      <li key={subIndex}>
+                        <Link href="/lesson">
+                          <div className="course-content-left">
+                            {lesson.isFree ? (
+                              <i className="feather-play-circle"></i>
+                            ) : (
+                              <i className="feather-lock"></i>
+                            )}
+                            <span className="text">{lesson.title}</span>
+                          </div>
+                          <div className="course-content-right">
+                            <span className="min-lable">{lesson.time}</span>
+                          </div>
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
               </div>
-            ))}
+            </div>
+          ))}
         </div>
       </div>
     </div>
